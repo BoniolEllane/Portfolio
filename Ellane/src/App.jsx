02eaa9a116c0a6a54b1b3d79f9ea632086099859
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-10-15 17:35:46 +0800

    I

--- Ellane/src/App.jsx
+++ Ellane/src/App.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+
+import React, { useState, useRef } from 'react';
+import { ReactTyped } from "react-typed";
+import './index.css';
 import image from '/src/assets/placeholder1.png';
 import ux1 from '/src/assets/ux.png';
 import { PiFacebookLogoThin, PiLinkedinLogoThin, PiPhoneThin } from "react-icons/pi";
 
-function App() {
+const App = () => {
   const [clickedCard, setClickedCard] = useState(null);
 
   const handleCardClick = (index) => {
     setClickedCard(clickedCard === index ? null : index); // Toggle the clicked card
   };
+
+
 
   return (
     <>
@@ -17,42 +22,86 @@ function App() {
           <div className="col">
             <div className="row_info">
               <p>PORTFOLIO</p>
-              <h1>CAROUSEL: FW / IT SPEC</h1>
+              <div className="type">
+                <h1>
+                  I am Ellane, <br /> {" "}
+                  <ReactTyped
+                    strings={["a Frontend Dev", "an IT Specialist", "a Web Developer"]}
+                    typeSpeed={150}
+                    loop
+                    backSpeed={50}
+                    cursorChar="|"
+                    showCursor={true}
+                  />
+                </h1>
+              </div>
               <h5>A passionate blabla</h5>
-              <button id="btn"><b>DOWNLOAD RESUME</b></button>
+              <a href="/src/assets/Boniol_Resume.pdf" download="Boniol_Resume.pdf">
+              <button id="btn" ><b>DOWNLOAD RESUME</b></button>
+              </a>
             </div>
             <div className="profile_image">
               <img src={image} alt="" />
             </div>
             <div className="row_social">
-              <div className="icon" id='facebook'><PiFacebookLogoThin /></div>
-              <div className="icon" id='Linkedin'><PiLinkedinLogoThin /></div>
-              <div className="icon" id='phone'><PiPhoneThin /></div>
+              <div className="icon" id='facebook'>
+                <a href="https://www.facebook.com/llanell29"><PiFacebookLogoThin /></a>
+                </div>
+              <div className="icon" id='Linkedin'>
+              <a href="https://www.linkedin.com/in/ellaneboniol/"><PiLinkedinLogoThin /></a>
+              </div>
+              <div className="icon" id='phone'>
+              <a href="tel:+639358784136"><PiPhoneThin /></a></div>
             </div>
           </div>
         </section>
 
         <section className="projects-slide-contain">
-          <h2>PREVIOUS WORKS</h2>
-          <p>Blablablabla</p>
+          <div className="proj-heading">
+            <h2>PREVIOUS WORKS</h2>
+            <p>These projects were.... Click the cards to show more details.</p>
+          </div>
+
           <hr />
           <div className="project-card">
-            {['Collaborative Design for UX/UI TUP Manila Website', 'Collaborative Work for Fil-Chi Job Fair 2024', 'ClearPath Website Design', 'The Writhing Labyrinth: A Wumpus Game', 'Ongpin Tower Website: On Progress'].map((title, index) => (
-              <div 
-                key={index} 
-                className={`card ${clickedCard === index ? 'active' : ''}`} 
-                onClick={() => handleCardClick(index)}
-              >
-                <div className="img-card">
-                  <img src={ux1} alt={title} />
+            {['Collaborative Design for UX/UI TUP Manila Website',
+              'Collaborative Work for Fil-Chi Job Fair 2024', 'ClearPath Website Design', 
+              'The Writhing Labyrinth: A Wumpus Game', 'Ongpin Tower Website: On Progress'].map((title, index) => (
+                <div
+                  key={index}
+                  className={`card ${clickedCard === index ? 'active' : ''}`}
+                  onClick={() => handleCardClick(index)}
+                >
+                  <div className="img-card">
+                    <img src={ux1} alt={title} />
+                  </div>
+                  <div className="colcard">
+                    <h3>{title}</h3>
+                    <p>Blabla</p>
+                    <button>Learn More</button>
+                  </div>
                 </div>
-                <div className="colcard">
-                  <h3>{title}</h3>
-                  <p>Blabla</p>
-                  <button>Learn More</button>
+              ))}
+          </div>
+          <div className="project-card">
+            {['Collaborative Design for UX/UI TUP Manila Website',
+              'Collaborative Work for Fil-Chi Job Fair 2024', 'ClearPath Website Design', 
+              'The Writhing Labyrinth: A Wumpus Game', 'Ongpin Tower Website: On Progress'].map((title, index) => (
+                <div
+                  key={index}
+                  className={`card ${clickedCard === index ? 'active' : ''}`}
+                  onClick={() => handleCardClick(index)}
+                >
+                  <div className="img-card">
+                    <img src={ux1} alt={title} />
+                  </div>
+                  <div className="colcard">
+                    <h3>{title}</h3>
+                    <p>Blabla</p>
+                    <button>Learn More</button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </section>
       </div>
