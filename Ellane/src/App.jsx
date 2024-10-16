16a7bb2480c299a05172a64aec6134d851e38e75
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-10-16 16:13:26 +0800

    [

--- Ellane/src/App.jsx
+++ Ellane/src/App.jsx
@@ -1,11 +1,14 @@
 
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { ReactTyped } from "react-typed";
 import './index.css';
 import image from '/src/assets/placeholder1.png';
 import ux1 from '/src/assets/ux.png';
-import { PiFacebookLogoThin, PiLinkedinLogoThin, PiPhoneThin } from "react-icons/pi";
-import { FaHtml5, FaBootstrap } from "react-icons/fa";
+import { PiLinkedinLogoThin, PiPhoneThin } from "react-icons/pi";
+import { FaGithub } from "react-icons/fa";
+import Marquee from "react-fast-marquee";
+
+
 
 const App = () => {
   const [clickedCard, setClickedCard] = useState(null);
@@ -24,7 +27,7 @@ const App = () => {
 
   return (
     <>
-      <div>
+      <div className='Main'>
         <section className="hero">
           <div className="col">
             <div className="row_info">
@@ -52,7 +55,7 @@ const App = () => {
             </div>
             <div className="row_social">
               <div className="icon" id='facebook'>
-                <a href="https://www.facebook.com/llanell29"><PiFacebookLogoThin /></a>
+                <a href="https://github.com/BoniolEllane"><FaGithub /></a>
               </div>
               <div className="icon" id='Linkedin'>
                 <a href="https://www.linkedin.com/in/ellaneboniol/"><PiLinkedinLogoThin /></a>
@@ -92,27 +95,166 @@ const App = () => {
           </div>
         </section>
 
+
         <section className="skills">
           <div className="col">
             <div className="skill_col1">
-              <h1>LANGUAGES:</h1>
-              <ul>
-                <li>Bootstrap</li>
-                <li>C++</li>
-              </ul>
-              BOOTSTRAP
-              C C++ C# JAVASCRIPT
-              HTML
-              REACTJS
-              CSS
-              FIGMA, Tailwind
+              <h1>TECHNICAL SKILLS:</h1>
+              <div className="lang">
+                <h3>Website Development</h3>
+              </div>
+              <div className="lang">
+                <h3>Website Designing</h3>
+              </div>
+              <div className="lang">
+                <h3>Frontend Development</h3>
+              </div>
+              <div className="lang">
+                <h3>Frontend Design</h3>
+              </div>
+              <div className="lang">
+                <h3>Networking</h3>
+              </div>
+              <div className="lang">
+                <h3>Game Design</h3>
+              </div>
+              <div className="lang">
+                <h3>UX/UI Design</h3>
+              </div>
+              <div className="lang">
+                <h3>Troubleshooting Skills</h3>
+              </div>
+              <div className="lang">
+                <h3>Systems Assessment</h3>
+              </div>
+
             </div>
             <div className="skill_col2">
-              Skills and Dev Tools
-              WordPress, Figma, Frontend Design, Frontend Development, Website Development, Networking, Game Design
-              Github, VSCode, Unity, Jupiter, PyCharm
+              <h1>CORE COMPETENCIES:</h1>
+              <div className="lang">
+                <h3>Effective in communication</h3>
+              </div>
+              <div className="lang">
+                <h3>Evaluative Thinking Skills</h3>
+              </div>
+              <div className="lang">
+                <h3>Highly motivated and proactive</h3>
+              </div>
+              <div className="lang">
+                <h3>Strong time management</h3>
+              </div>
             </div>
           </div>
+          <div className="skills-marquee">
+          <h3>LANGUANGES:</h3>
+            <Marquee
+              autoFill={false}
+              pauseOnHover={true}
+              speed={100}
+              gradient={false}
+              direction={"right"}
+              style={{
+                marginBottom:'5em',
+              }}
+            >
+              <div className="technical_marquee">
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000" alt="Bootstrap Icon" />
+                  BOOTSTRAP
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=shQTXiDQiQVR&format=png&color=000000" alt="C Icon" />
+                  C LANGUAGE
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=40669&format=png&color=000000" alt="C++ Icon" />
+                  C++ LANGUAGE
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=55251&format=png&color=000000" alt="C# Icon" />
+                  C# LANGUAGE
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=21278&format=png&color=000000" alt="CSS Icon" />
+                  CSS
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=108784&format=png&color=000000" alt="JavaScript Icon" />
+                  JAVASCRIPT
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=20909&format=png&color=000000" alt="HTML Icon" />
+                  HTML
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=123603&format=png&color=000000" alt="ReactJS Icon" />
+                  REACTJS
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=x7XMNGh2vdqA&format=png&color=000000" alt="Tailwind Icon" />
+                  TAILWIND
+                </div>
+
+              </div>
+            </Marquee>
+            
+            <h3>DEVELOPER TOOLS:</h3>
+            <Marquee
+            autoFill={true}
+              pauseOnHover={true}
+              speed={100}
+              gradient={false}
+              direction={"left"}
+              style={{
+                whiteSpace: 'nowrap',
+                display: 'flex',
+                gap: '20px'
+              }}
+            >
+              <div className="technical_marquee">
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=13664&format=png&color=000000" alt="WordPress Icon" />
+                  WORDPRESS
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=zfHRZ6i1Wg0U&format=png&color=000000" alt="Figma Icon" />
+                  FIGMA
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=AZOZNnY73haj&format=png&color=ffffff" alt="GitHub Icon" />
+                  GITHUB
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=9OGIyU8hrxW5&format=png&color=000000" alt="VSCode Icon" />
+                  VSCODE
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=J0SgMWzAxqFj&format=png&color=000000" alt="Jupyter Icon" />
+                  JUPYTER
+                </div>
+
+                <div className='marq_item'>
+                  <img src="https://img.icons8.com/?size=100&id=vinpBD5oA3b4&format=png&color=000000" alt="PyCharm Icon" />
+                  PYCHARM
+                </div>
+
+              </div>
+            </Marquee>
+          </div>
+          
         </section>
       </div>
     </>
